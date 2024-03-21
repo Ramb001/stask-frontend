503a51e784d5081cf6436af5bbbe6a7880bfbc59
Artem <artem.rmb@bk.ru>
2024-03-21 21:17:18 +0300

    feat: created routes for page

--- src/components/OrganizationCard/index.tsx
+++ src/components/OrganizationCard/index.tsx
@@ -1,10 +1,25 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import GoIcon from "../../assets/icons/GoIcon";
 import { OrganizationInterface } from "../../constants";
 import styles from "./OrganixationCard.module.scss";
 
 function OrganizationCard({ params }: { params: OrganizationInterface }) {
+  const [searchParams, _] = useSearchParams();
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.card}>
+    <div
+      className={styles.card}
+      onClick={() =>
+        navigate(
+          `/${params.name}/tasks?user_id=${searchParams.get(
+            "user_id"
+          )}&language_code=${searchParams.get("language_code")}&user_status=${
+            params.user_status
+          }`
+        )
+      }
+    >
       <div className={styles.mainInfo}>
         <div className={styles.title}>{params.name}</div>
         <div className={styles.subtext}>{params.user_status}</div>
