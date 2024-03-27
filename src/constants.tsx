077bfbbe62f0ef46c61e3e075c6d495f074e7b83
Artem <artem.rmb@bk.ru>
2024-03-27 21:39:57 +0300

    feat: added new constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -4,3 +4,23 @@ export interface OrganizationInterface {
   name: string;
   user_status: string;
 }
+
+export interface TaskInterface {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+  workers: Array<{ name: string; value: "name" | "username" }>;
+  deadline: string;
+}
+
+interface statusesInterface {
+  name: "Not Started" | "In progress" | "Done";
+  value: "not_started" | "in_progress" | "done";
+}
+
+export const statuses: Array<statusesInterface> = [
+  { name: "Not Started", value: "not_started" },
+  { name: "In progress", value: "in_progress" },
+  { name: "Done", value: "done" },
+];
