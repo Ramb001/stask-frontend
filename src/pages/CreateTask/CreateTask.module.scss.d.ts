620b6afd3392b8d5bbb85d4896160a573a39c448
Artem <artem.rmb@bk.ru>
2024-03-29 19:40:33 +0300

    feat: created page for creating tasks

---
+++ src/pages/CreateTask/CreateTask.module.scss.d.ts
@@ -0,0 +1,5 @@
+export const page: string;
+export const title: string;
+export const inputDesc: string;
+export const select: string;
+export const dateInput: string;
--- src/pages/CreateTask/index.tsx
+++ src/pages/CreateTask/index.tsx
@@ -1,12 +1,27 @@
-import { useCallback, useContext, useEffect } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { WebAppContext } from "../../contexts/WebAppContext";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import styles from "./CreateTask.module.scss";
+import {
+  WorkersInterface,
+  maxNumberOfDescriptionCharaters,
+  url,
+} from "../../constants";
+import axios from "axios";
+import InputLayout from "../../layouts/InputLayout";
+import { Option, Select, Textarea, Typography } from "@mui/joy";
+import InputMask from "react-input-mask";
 
 function CreateTask() {
   const { WebApp } = useContext(WebAppContext);
   const [searchParams, _] = useSearchParams();
   const navigate = useNavigate();
   const params = useParams();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [workers, setWorkers] = useState<Array<string>>([]);
+  const [deadline, setDeadline] = useState("");
 
   const sendDataToTelegram = useCallback(() => {
     {
@@ -15,31 +30,16 @@ function CreateTask() {
           action: "create_task",
           data: {
             organization: params.organizationName,
-            title: "Test new task",
-            description: "test description",
+            title: title,
+            description: description,
             creator: searchParams.get("user_id"),
-            workers: [
-              {
-                id: "zktal3nexiy053x",
-                name: "Artem Stanko",
-                username: "artem.rmb",
-                tg_id: "440758448",
-                chat_id: "440758448",
-              },
-              {
-                id: "2bur69x384lz39a",
-                name: "",
-                username: "tonlance_support",
-                tg_id: "5907442895",
-                chat_id: "5907442895",
-              },
-            ],
-            deadline: "test",
+            workers: workers,
+            deadline: deadline,
           },
         })
       );
     }
-  }, [WebApp]);
+  }, [WebApp, title, description, workers, deadline]);
 
   const backToTasks = useCallback(() => {
     WebApp?.MainButton.hide();
@@ -74,7 +74,89 @@ function CreateTask() {
     WebApp?.MainButton.setText("Create");
   }, [WebApp]);
 
-  return <>{params.organizationName}</>;
+  const [allWorkers, setAllWorkers] = useState<Array<WorkersInterface>>([]);
+
+  useEffect(() => {
+    axios
+      .get(`${url}/get-workers`, {
+        params: { organization: params.organizationName },
+      })
+      .then((resp) => {
+        setAllWorkers(resp.data);
+      });
+  }, []);
+
+  function handleChangeWorkers(
+    event: React.SyntheticEvent | null,
+    newValue: string | null
+  ) {
+    if (newValue) {
+      const temp: string[] = [...newValue];
+      setWorkers(temp);
+    }
+  }
+
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.title}>
+        Create <br /> Task
+      </h1>
+      <InputLayout
+        props={{
+          type: "text",
+          title: "",
+          placeholder: "Task title",
+          value: title,
+          setValue: setTitle,
+        }}
+      />
+      <Textarea
+        placeholder="Description"
+        slotProps={{
+          textarea: {
+            pattern: "[A-Za-z]",
+          },
+        }}
+        className={styles.inputDesc}
+        endDecorator={
+          <Typography
+            fontSize={12}
+            sx={{ ml: "auto", color: "var(--tg-theme-text-color)" }}
+          >
+            {description.length}/{maxNumberOfDescriptionCharaters} character(s)
+          </Typography>
+        }
+        value={description}
+        onChange={(e) => {
+          e.target.value.length <= maxNumberOfDescriptionCharaters
+            ? setDescription(e.target.value)
+            : "";
+        }}
+      />
+      <Select
+        className={styles.select}
+        placeholder="Select workers"
+        multiple={true}
+        value={workers}
+        onChange={handleChangeWorkers}
+      >
+        {allWorkers.map((worker, idx) => {
+          return (
+            <Option value={worker} key={idx}>{`${
+              worker.name !== "" ? "" : "@"
+            }${worker.name !== "" ? worker.name : worker.username}`}</Option>
+          );
+        })}
+      </Select>
+      <InputMask
+        mask="99/99/9999"
+        placeholder="Deadline"
+        value={deadline}
+        onChange={(e: any) => setDeadline(e.target.value)}
+        className={styles.dateInput}
+      />
+    </div>
+  );
 }
 
 export default CreateTask;
